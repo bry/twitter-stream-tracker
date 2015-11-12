6e99c3fcbf61184f37d83f0f81c9960cf188babd
Bryan B. Cabalo <bcabalo@gmail.com>
2015-11-11 18:00:09 -0800

    Attach Tracker methods to its eigenclass, need only one instance of Tracker

--- lib/tracker.rb
+++ lib/tracker.rb
@@ -1,50 +1,51 @@
 module TwitterStream
   class Tracker
-    def initialize
-      @total_word_count = 0
-      @word_count = {}
-    end
+    @total_word_count = 0
+    @word_count = {}
 
-    def process(tweet)
-      add_word_count(tweet)
-      add_words_to_hash(tweet)
-    end
+    class << self
+      def process(tweet)
+        add_word_count(tweet)
+        add_words_to_hash(tweet)
+      end
 
-    def add_word_count(tweet)
-      @total_word_count += tweet.word_count
-    end
+      def add_word_count(tweet)
+        @total_word_count += tweet.word_count
+      end
 
-    def add_words_to_hash(tweet)
-      tweet.filter_stop_words.each do |word|
-        if word_count[word.to_sym].nil?
-          word_count[word.to_sym] = 1
-        else
-          word_count[word.to_sym] += 1
+      def add_words_to_hash(tweet)
+        tweet.filter_stop_words.each do |word|
+          if word_count[word.to_sym].nil?
+            word_count[word.to_sym] = 1
+          else
+            word_count[word.to_sym] += 1
+          end
         end
       end
-    end
 
-    def print_results
-      puts
-      puts "***********Results***********"
-      puts "Total Word Count: #{@total_word_count}"
-      puts
-      puts "Ten Most Frequent Words:"
-      top(10).each do |key, value|
-        puts "#{key}: #{value}"
+      def print_results
+        puts
+        puts "***********Results***********"
+        puts "Total Word Count: #{@total_word_count}"
+        puts
+        puts "Ten Most Frequent Words:"
+        top(10).each do |key, value|
+          puts "#{key}: #{value}"
+        end
       end
-    end
 
-    private
+      private
 
-    def top(number)
-      top_words = word_count.sort do |a, b|
-        b[1] <=> a[1]
-      end.first(number)
-    end
+      def top(number)
+        word_count.sort do |a, b|
+          b[1] <=> a[1]
+        end.first(number)
+      end
 
-    def word_count
-      @word_count
+      def word_count
+        @word_count
+      end
+
     end
   end
 end
--- tracker.rb
+++ tracker.rb
@@ -6,19 +6,18 @@ require_relative 'lib/tracker'
 # Use Event Machine for timer program reporting and exit
 EM.run do
   client = TweetStream::Client.new
-  tracker = TwitterStream::Tracker.new
 
   # Set Periodic Timer to exit program and report results after 5 minutes
   EM::PeriodicTimer.new(TwitterStream::TERM_IN_SECONDS) do
     client.stop
-    tracker.print_results
+    TwitterStream::Tracker.print_results
     exit
   end
 
   # Receive sample stream from Twitter and use Tracker::TweetTracker to parse
   client.sample do |status|
     tweet = TwitterStream::Tweet.new(status.text)
-    tracker.process(tweet)
+    TwitterStream::Tracker.process(tweet)
     puts tweet
   end
 end
